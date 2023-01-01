0839f993a6a7fd9ac4c985420a4373853ead46b5
reyhanetahernezhad <reyhane.tahernezhad2@gmail.com>
2023-01-01 12:45:11 +0330

    UPDATE models

--- residences/models.py
+++ residences/models.py
@@ -15,12 +15,20 @@ class AbstractResidence(models.Model):
 
 class Room(AbstractResidence):
 
-    class_choices = (
+    facility_choices = (
        ('1', 'parking'),
        ('2', 'breakfast'),
        ('3', 'fitness center'),
        ('4', 'free wifi'),
 
+    )
+
+    star_choices = (
+        ('one_star', '1_star'),
+        ('two_star', '2_star'),
+        ('three_star', '3_star'),
+        ('four_star', '4_star'),
+        ('five_star', '5_star'),
     )
 
     room_num = models.SmallIntegerField()
@@ -29,4 +37,5 @@ class Room(AbstractResidence):
     is_booked = models.BooleanField(default=False)
     checkin = models.BooleanField(default=False)
     checkout = models.BooleanField(default=False)
-    facility = models.CharField(choices=class_choices, max_length=30)
+    facility = models.CharField(choices=facility_choices, max_length=30)
+    rate = models.CharField(choices=star_choices, max_length=30)
