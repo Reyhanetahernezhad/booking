523ea9d3134311ed50629d7a4f877e78c8bd9c9c
reyhanetahernezhad <reyhane.tahernezhad2@gmail.com>
2023-01-05 13:07:53 +0330

    ADD filtering to residences api

--- residences/api/views.py
+++ residences/api/views.py
@@ -1,16 +1,16 @@
-# from residences.api.serializer import HotelRoomSerializer
-# from rest_framework import viewsets, mixins
-# from residences.models import HotelRoom
-# from django_filters import rest_framework as filters
-# from filter.filtering import HotelRoomFilter
-#
-#
-# class HotelRoomViewSet(mixins.ListModelMixin,
-#                        mixins.RetrieveModelMixin,
-#                        viewsets.GenericViewSet):
-#
-#     queryset = HotelRoom.objects.all()
-#     serializer_class = HotelRoomSerializer
-#     filter_backends = (filters.DjangoFilterBackend,)
-#     filterset_class = HotelRoomFilter
+from residences.api.serializer import HotelRoomSerializer
+from rest_framework import viewsets, mixins
+from residences.models import HotelRoom
+from django_filters import rest_framework as filters
+from filter.filtering import HotelRoomFilter
+
+
+class HotelRoomViewSet(mixins.ListModelMixin,
+                       mixins.RetrieveModelMixin,
+                       viewsets.GenericViewSet):
+
+    queryset = HotelRoom.objects.all()
+    serializer_class = HotelRoomSerializer
+    filter_backends = (filters.DjangoFilterBackend,)
+    filterset_class = HotelRoomFilter
 
