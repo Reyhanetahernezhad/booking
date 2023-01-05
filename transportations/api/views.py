c83566b4556889f3568cd9f7708f2152c4c36a53
reyhanetahernezhad <reyhane.tahernezhad2@gmail.com>
2023-01-05 13:17:25 +0330

    ADD filtering to transportations api

--- transportations/api/views.py
+++ transportations/api/views.py
@@ -1,8 +1,16 @@
-from rest_framework import generics
-from .serializer import *
-from transportations.models import *
+from residences.api.serializer import HotelRoomSerializer
+from rest_framework import viewsets, mixins
+from residences.models import HotelRoom
+from django_filters import rest_framework as filters
+from filter.filtering import AirplaneFilter
+from transportations.api.serializer import AirplaneSerializer
+from transportations.models import Airplane
 
 
-class ListAirplane(generics.ListAPIView):
+class AirplaneViewSet(mixins.ListModelMixin,
+                      mixins.RetrieveModelMixin,
+                      viewsets.GenericViewSet):
     queryset = Airplane.objects.all()
     serializer_class = AirplaneSerializer
+    filter_backends = (filters.DjangoFilterBackend,)
+    filterset_class = AirplaneFilter
